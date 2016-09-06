4f8e15b765676222cb9aab30943dc1f84dc305e1
Justin Doak <justinhdoak@gmail.com>
2016-09-06 18:28:42 -0400

    Fixed signup issue in signup.js controller

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -87,18 +87,18 @@ angular.module('myApp')
       this.submitted = true;
 
       if (form.$valid) {
-        return this.Auth.createUser({
+        return Auth.createUser({
           name: this.user.name,
           email: this.user.email,
           password: this.user.password
         })
         .then(() => {
           // Account created, redirect to home
-          this.$state.go('home');
+          $state.go('home');
         })
         .catch(err => {
           err = err.data;
-          this.errors = {};
+          errors = {};
           // Update validity of form fields that match the mongoose errors
           angular.forEach(err.errors, (error, field) => {
             form[field].$setValidity('mongoose', false);
