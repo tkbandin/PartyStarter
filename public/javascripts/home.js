852e3365c290813f05f9954be1b9e3de41b4bc84
Kirsten Humphreys <kirstenrhumphreys@gmail.com>
2016-09-09 11:37:14 -0400

    mock-layouts updated

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,12 +1,23 @@
 angular.module('myApp')
 .component('home', {
   template: `
-  <section class="container well text-center">
-    <h1>Welcome to the</h1>
-    <h1>{{ $ctrl.name }}</h1>
-  </section>
+  <div class='main-landing'>
+    <i class="material-icons">insert_emoticon</i>
+    <h2>Hey!</h2>
+    <h3>Welcome to the {{ $ctrl.name }}</h3>
+    <p>This is an explanation and it is super exciting and makes you want to part and use our app. It is really cool because you can do a lot of stuff.</p>
+    <a class="waves-effect waves-light btn" href="!#">Get Started</a>
+  </div>
   `,
   controller: function() {
     this.name = 'Party App!';
   }
 });
+
+
+
+//Old Section Tag
+// <section class="container well text-center">
+//   <h1>Welcome to the</h1>
+//   <h1>{{ $ctrl.name }}</h1>
+// </section>
