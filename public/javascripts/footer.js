7dd6bdc7226257d1680a7e147a8f6bd793e7650f
Justin Doak <justinhdoak@gmail.com>
2016-09-10 22:09:11 -0400

    Restyled and minimized footer.

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -2,31 +2,26 @@ angular.module('myApp')
 .component('appFooter', {
   template: `
       <footer class="page-footer teal lighten-5">
-        <div class="container">
-          <div class="row">
 
-            <div class="col l6 s12">
-              <h5 class="black-text">About PartyStarter</h5>
+          <div class="row">
+            <div class="col s12">
               <p class="black-text">Created by Group 5 for Project 3 of General Assembly's Web Development Immersive.</p>
             </div>
-
-            <div class="col l4 offset-l2 s12">
-              <h5 class="black-text">Contact</h5>
-              <ul>
-                <li><a class="black-text" href="https://github.com/jhdoak">Justin Doak</a></li>
-                <li><a class="black-text" href="https://github.com/kirstenhumphreys">Kirsten Humphreys</a></li>
-                <li><a class="black-text" href="https://github.com/sammershon">Sam Mershon</a></li>
-                <li><a class="black-text" href="https://github.com/joshpereira">Josh Pereira</a></li>
-              </ul>
-            </div>
-
           </div>
-        </div>
+
+            <div class='divider'></div>
+
+            <ul class='row'>
+              <li class='col s3'><a class="black-text" href="https://github.com/jhdoak">Justin Doak</a></li>
+              <li class='col s3'><a class="black-text" href="https://github.com/kirstenhumphreys">Kirsten Humphreys</a></li>
+              <li class='col s3'><a class="black-text" href="https://github.com/sammershon">Sam Mershon</a></li>
+              <li class='col s3'><a class="black-text" href="https://github.com/joshpereira">Josh Pereira</a></li>
+            </ul>
+
+
+
         <div class="footer-copyright">
-          <div class="container">
-          View our project on <a href="https://github.com/sammershon/Project3">GitHub</a>.</p>
-          <a class="black-text text-lighten-1 right" href="#!">More Links</a>
-          </div>
+          <p>View our project on <a href="https://github.com/sammershon/Project3">GitHub</a>.</p>
         </div>
       </footer>
   `
