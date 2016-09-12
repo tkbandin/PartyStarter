b38dbd458d8259eeb9868516c19b357b3629ece0
Sam Mershon <samrmershon@gmail.com>
2016-09-12 14:58:29 -0400

    fixed map page

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -35,46 +35,51 @@ angular.module('myApp')
             }
         ];
 
-        var map;
-        function initMap() {
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-          });
-        }
+        var mapOptions = {
+                  zoom: 11,
+                  center: new google.maps.LatLng(33.7490,-84.3880),
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+              }
 
-      $scope.markers = [];
+              $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-      var infoWindow = new google.maps.InfoWindow();
+              $scope.markers = [];
+
+              var infoWindow = new google.maps.InfoWindow();
+
+              var createMarker = function (info){
+
+                  //Marker settings
+                  var marker = new google.maps.Marker({
+                      map: $scope.map,
+                      position: new google.maps.LatLng(info.lat, info.long),
+                      title: info.place,
+                      animation: google.maps.Animation.DROP
+                  });
+                  //Set info window content
+                  marker.content = '<div class="infoWindowContent">' + info.desc + '<br />' + '<br />' + info.address + '<br />' + info.lat + ' E, ' + info.long +  ' N </div>';
+
+                  //When marker clicked, display info window while zooming in on party location
+                  google.maps.event.addListener(marker, 'click', function(){
+                      infoWindow.setContent('<h2>' + marker.title + '</h2>' +
+                        marker.content);
+                      infoWindow.open($scope.map, marker);
+                      $scope.map.setZoom(15);
+                      $scope.map.setCenter(marker.getPosition());
+                  });
+
+                  $scope.markers.push(marker);
+
+              }
+
+              for (i = 0; i < parties.length; i++){
+                  createMarker(parties[i]);
+              }
 
       //function to open window when party link is clicked
       $scope.openInfoWindow = function(e, selectedMarker){
                   e.preventDefault();
                   google.maps.event.trigger(selectedMarker, 'click');
               }
-
-      var createMarker = function (info){
-
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              position: new google.maps.LatLng(info.lat, info.long),
-              title: info.place
-          });
-          marker.content = '<div class="infoWindowContent">' + info.desc + '<br />' + '<br />' + info.lat + ' E,' + info.long +  ' N, </div>';
-
-          google.maps.event.addListener(marker, 'click', function(){
-              infoWindow.setContent('<h4>' + marker.title + '</h4>' +
-              marker.content);
-              infoWindow.open($scope.map, marker);
-          });
-
-          $scope.markers.push(marker);
-
-      }
-
-      for (i = 0; i < parties.length; i++){
-          createMarker(parties[i]);
-      }
-
   }
   });
