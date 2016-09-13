0abf7d0aa92dec55e6dec32ea026618163359e6a
Justin Doak <justinhdoak@gmail.com>
2016-09-13 18:35:49 -0400

    Added mongoDB sandbox and connected in app.js and seeds.js.

--- app.js
+++ app.js
@@ -17,8 +17,21 @@ var partiesRouter = require('./routes/parties');
 
 var app = express();
 
-// Connection to database goes here
-mongoose.connect('mongodb://localhost/partystarter');
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/partystarter');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
