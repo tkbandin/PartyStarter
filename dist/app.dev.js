ca7d745ec779e543701854b9b4c7125a32de19da
Terra Bandin <tkbandin@gmail.com>
2020-08-25 21:16:48 -0700

    add Procfile and set db address

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,96 +1,101 @@
+"use strict";
+
 var express = require("express");
+
 var path = require("path");
+
 var favicon = require("serve-favicon");
+
 var logger = require("morgan");
+
 var cookieParser = require("cookie-parser");
+
 var bodyParser = require("body-parser");
+
 var mongoose = require("mongoose");
+
 mongoose.Promise = require("bluebird").Promise;
 
 var passport = require("passport");
-var session = require("express-session");
 
-// Routes
+var session = require("express-session"); // Routes
+
+
 var homeRouter = require("./routes/index");
+
 var partiesRouter = require("./routes/parties");
 
-var app = express();
-
-// Connect to database
+var app = express(); // Connect to database
 // if (process.env.MONGODB_URI) {
 //   mongoose.connect(process.env.MONGODB_URI);
 // }
 // else {
-mongoose.connect(
-  "mongodb+srv://partymash-admin:cityuproject123!@cluster0.6hwhq.mongodb.net/partymash?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-// mongoose.connect('mongodb://localhost/partymash');
+
+mongoose.connect("mongodb+srv://partymash-admin:cityuproject123!@cluster0.6hwhq.mongodb.net/partymash?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); // mongoose.connect('mongodb://localhost/partymash');
 // }
+
 mongoose.connection.on("error", function (err) {
   console.error("\nMongoDB connection error: " + err);
   process.exit(-1);
 });
 mongoose.connection.once("open", function () {
   console.log("\nMongoose has connected to MongoDB!");
-});
+}); // view engine setup
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-// uncomment after placing your favicon in /public
+app.set("view engine", "ejs"); // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
-
-app.use(
-  session({ secret: "WDI Rocks!", resave: true, saveUninitialized: true })
-);
+app.use(session({
+  secret: "WDI Rocks!",
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); // Configure passport with our custom configuration code
 
-// Configure passport with our custom configuration code
-require("./config/passport/passport")(passport);
+require("./config/passport/passport")(passport); // Routes
 
-// Routes
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express["static"](path.join(__dirname, "public")));
 app.use("/", homeRouter);
-app.use("/parties", partiesRouter);
+app.use("/parties", partiesRouter); // catch 404 and forward to error handler
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
-});
-
-// error handlers
-
+}); // error handlers
 // development error handler
 // will print stacktrace
+
 if (app.get("env") === "development") {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
-      error: err,
+      error: err
     });
   });
-}
-
-// production error handler
+} // production error handler
 // no stacktraces leaked to user
+
+
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,
-    error: {},
+    error: {}
   });
 });
-
 console.log("Running in %s mode", app.get("env"));
-
 module.exports = app;
